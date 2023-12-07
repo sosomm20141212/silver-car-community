448a15551afec7bfc974d626b32da2e37b9dc507
sosomm20141212 <sosomm20141212@gmail.com>
2023-12-07 19:03:01 +0900

    등록된 업체 java crawling celenium

---
+++ _data_analysis/sosomm20141212/sprint_project/sprint/src/test/java/com/example/sprint/SprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
