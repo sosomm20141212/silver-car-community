7ec0e8b0a4d8a0afb4324659bee828f0e0f72723
sosomm20141212 <sosomm20141212@gmail.com>
2023-12-12 09:30:54 +0900

    테스트로 올린 client파일 제거

--- _data_analysis/sosomm20141212/client/server/routes/index.js
+++
@@ -1,7 +0,0 @@
-// const express = require('express');
-// const app = express();
-// const PORT = process.env.PORT || 4000;
-
-// app.listen(PORT, () => {
-//     console.log(`서버가 ${PORT}번 포트에서 실행 중 입니다`)
-// })
