12356569244ff1649fd6e079103c9ca637417496
DESKTOP-QTMOPTO\user <notrowing@naver.com>
2023-12-13 19:36:28 +0900

    홈페이지 수정 압축 파일

--- _data_analysis/notrowing/조정아의 서버5/server/src/main/java/com/kepco/scc/repository/VehicleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kepco.scc.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kepco.scc.model.Vehicle;
-
-public interface VehicleRepository extends JpaRepository<Vehicle,String>{
-    
-}
