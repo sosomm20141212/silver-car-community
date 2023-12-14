4c58e6ea59c49ef348f6d6f674a353c5a9a759ca
sosomm20141212 <sosomm20141212@gmail.com>
2023-12-14 09:14:08 +0900

    register css id 수정

--- _data_analysis/sosomm20141212/client-register/src/componenets/Register.js
+++ _data_analysis/sosomm20141212/client-register/src/componenets/Register.js
@@ -23,49 +23,49 @@ function Register() {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="register-form">
-      <h1>회원가입</h1>
+      <h1 className="register-h1">회원가입</h1>
 
-      <label htmlFor="userId">아이디</label>
+      <label htmlFor="userId" className="register-label">아이디</label>
       <Controller
         name="userId"
         control={control}
         defaultValue=""
         rules={{ required: "아이디를 입력해주세요" }}
-        render={({ field }) => <input id="userId" type="text" {...field} />}
+        render={({ field }) => <input className="register-input" id="userId" type="text" {...field} />}
       />
       {formState.errors.userId && <p>{formState.errors.userId.message}</p>}
 
-      <label htmlFor="password">비밀번호</label>
+      <label htmlFor="password" className="register-label">비밀번호</label>
       <Controller
         name="password"
         control={control}
         defaultValue=""
         rules={{ required: "비밀번호를 입력해주세요" }}
-        render={({ field }) => <input id="password" type="password" {...field} />}
+        render={({ field }) => <input className="register-input" id="password" type="password" {...field} />}
       />
       {formState.errors.password && <p>{formState.errors.password.message}</p>}
 
-      <label htmlFor="confirmPassword">비밀번호 확인</label>
+      <label htmlFor="confirmPassword" className="register-label">비밀번호 확인</label>
       <Controller
         name="confirmPassword"
         control={control}
         defaultValue=""
         rules={{ required: "비밀번호를 다시 한번 입력해주세요" }}
-        render={({ field }) => <input id="confirmPassword" type="password" {...field} />}
+        render={({ field }) => <input className="register-input" id="confirmPassword" type="password" {...field} />}
       />
       {formState.errors.confirmPassword && <p>{formState.errors.confirmPassword.message}</p>}
 
-      <label htmlFor="email">이메일 주소</label>
+      <label htmlFor="email" className="register-label">이메일 주소</label>
       <Controller
         name="email"
         control={control}
         defaultValue=""
         rules={{ required: "이메일을 입력해주세요" }}
-        render={({ field }) => <input id="email" type="email" {...field} />}
+        render={({ field }) => <input className="register-input" id="email" type="email" {...field} />}
       />
       {formState.errors.email && <p>{formState.errors.email.message}</p>}
 
-      <button type="submit">회원가입</button>
+      <button type="submit" className="register-button">회원가입</button>
     </form>
   );
 }
