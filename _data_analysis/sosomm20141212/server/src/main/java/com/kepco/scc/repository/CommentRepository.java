88911fb40bea46474a11056aae331e77f85f40f5
raincross7 <raincross7@gmail.com>
2023-12-15 10:41:32 +0900

    data_analysis 안의 임시 파일 삭제

--- _data_analysis/sosomm20141212/server/src/main/java/com/kepco/scc/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kepco.scc.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kepco.scc.model.Comment;
-
-public interface CommentRepository extends JpaRepository<Comment,Integer>{
-    
-}
